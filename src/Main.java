bf3cb240136510584517dc074a2dc8fedc3d0f5b
RW <rinze.watanabe.translation@gmail.com>
2019-06-28 15:54:21 +1000

    Add feature

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,55 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void showFiles(String directory) {
+    private static void URLtoTXT(String directory) throws IOException {
         File dir = new File(directory);
-        File[] directoryListing = dir.listFiles();
-        System.out.println(directoryListing);
+        File[] listOfFiles = dir.listFiles();
+        ArrayList<String> url = new ArrayList<>();
+        assert listOfFiles != null;
+
+        for (File listOfFile : listOfFiles) {
+            if (listOfFile.isFile()) {
+
+                try (BufferedReader br = new BufferedReader(new FileReader(listOfFile))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        // process the line.
+                        try {
+                            if (line.substring(0, 4).equals("URL=")) {
+                                url.add(line.substring(4));
+                            }
+                        } catch (NullPointerException ignored) {
+
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        editTxt(dir, url);
 
     }
 
-    public static void main(String[] args) {
-	// write your code here
-        showFiles("C:\\Users\\RW\\Desktop\\code\\compass\\compass");
+    private static void editTxt(File directory, ArrayList<String> urlList) throws IOException {
+        Path file = directory.toPath();
+        file = (Path) Paths.get(file.toString(), "url.txt");
+        Files.write(file, urlList, StandardCharsets.UTF_8);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        URLtoTXT(args[0]);
     }
 }
